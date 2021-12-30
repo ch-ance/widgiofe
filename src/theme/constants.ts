3fbd2b1c4d261ecccc4f816c51b1d44627b70c30
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2021-12-30 15:16:28 -0600

    WM-4 adds theme folder and some constants for colors. This was a short implementation and alterations are welcomed kindly

---
+++ src/theme/constants.ts
@@ -0,0 +1,10 @@
+// https://coolors.co/0fa3b1-931621-171614-eac435-6610f2
+const colors = {
+  "blue-green": "0fa3b1",
+  "ruby-ref": "931621",
+  "eerie-black": "171614",
+  "saffron-yellow": "eac435",
+  "electric-indigo": "6610f2",
+};
+
+export { colors };
