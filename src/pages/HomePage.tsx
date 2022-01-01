dbdfc14ca914b42c474e06717fcf77a9568505a9
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2022-01-01 08:43:55 -0600

    WM-7: add Talkfree and several dummy widgets to help test grid layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,51 @@
-import Page, { Grid, GridColumn } from "@atlaskit/page";
+/** @jsxImportSource @emotion/react */
+
+import { css } from "@emotion/react";
+import { useEffect, useState } from "react";
 import Header from "../components/molecules/Header";
+import { WidgetMetadata } from "../components/templates/types";
+import WidgetsContainer from "../components/templates/WidgetsContainer";
+import { colors } from "../theme/constants";
+
+const PageStyle = css({
+  backgroundColor: colors["eerie-black"],
+  color: colors.text,
+  height: "100vh",
+  maxHeight: "100vh",
+});
+
 const HomePage = () => {
+  const [widgets, setWidgets] = useState<WidgetMetadata[]>();
+
+  useEffect(() => {
+    const widgetData = [
+      { name: "talkfree", fileName: "Talkfree" },
+      { name: "another widget", fileName: "1" },
+      { name: "another widget 2", fileName: "2" },
+      { name: "another widget 3", fileName: "3" },
+    ];
+    setWidgets(widgetData);
+  }, []);
+
   return (
-    <Page testId="home-page">
-      <Grid testId="home-page-grid">
-        <GridColumn medium={12}>
-          <Header title="WidgyMa Dashboard" />
-        </GridColumn>
-        <GridColumn medium={8}></GridColumn>
-        <GridColumn medium={4}></GridColumn>
-        <GridColumn></GridColumn>
-      </Grid>
-    </Page>
+    <div css={PageStyle}>
+      <Header />
+      <main>
+        {widgets ? (
+          <WidgetsContainer widgets={widgets} />
+        ) : (
+          <h2>loading widgets...</h2>
+        )}
+      </main>
+      <footer
+        style={{
+          position: "absolute",
+          bottom: 0,
+        }}
+      >
+        no animals were harmed in the making of this site. all rights reserved.
+      </footer>
+    </div>
   );
 };
 
